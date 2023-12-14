63f5516620283562e153b556495512989acf2242
saltyee <seanvsyoyo@gmail.com>
2023-12-14 10:33:42 +0800

    Reg_PC update

--- Reg_PC.sv
+++ Reg_PC.sv
@@ -4,7 +4,7 @@ input reset,
 input [31:0] next_pc,
 output reg [31:0] current_pc
 );
-always @(posedge clk or negedge reset) begin
+always @(negedge clk or negedge reset) begin
     if(!reset)
         current_pc <= 32'd0;
     else
