d0e67ecb8bd5f0c2a0d421fdb0486757643a3c08
saltyee <seanvsyoyo@gmail.com>
2023-12-14 12:37:51 +0800

    pipeline reg done

--- E_M_Reg.v
+++ E_M_Reg.v
@@ -1,6 +1,7 @@
 module E_M_Reg (
 	input clk,
 	input reset,
+	input flush,
 	input [31:0]alu_out,
 	input [31:0]rs2_data,
 	input [4:0]rd_index,
@@ -26,9 +27,43 @@ module E_M_Reg (
 );
 
 always@(posedge clk or negedge rst) begin
-	if(rst)
-		
+	if(!rst) begin
+		alu_out_reg <= 32'b0;
+		rs2_data_reg <= 32'b0;
+		rd_index_reg <= 5'b0;
+		jb_addr_reg <= 32'b0;
+		/*control signal*/
+		branch_taken_reg <= 1'b0;
+		dm_w_en_reg <= 4'b0;
+		halt_reg <= 1'b0;
+		wb_sel_reg <= 1'b0;
+		wb_en_reg <= 1'b0;
+		func3_reg <= 3'b0;
 	end
+	else begin
+		alu_out_reg <= alu_out;
+		rs2_data_reg <= rs2_data;
+		rd_index_reg <= rd_index;
+		jb_addr_reg <= jb_addr;
+		branch_taken_reg <= branch_taken;
+		if(flush) begin
+			branch_taken_reg <= 1'b0;
+			dm_w_en_reg <= 4'b0;
+			halt_reg <= 1'b0;
+			wb_sel_reg <= 1'b0;
+			wb_en_reg <= 1'b0;
+			func3_reg <= 3'b0;
+		end
+		else begin
+			branch_taken_reg <= branch_taken;
+			dm_w_en_reg <= dm_w_en;
+			halt_reg <= halt;
+			wb_sel_reg <= wb_sel;
+			wb_en_reg <= wb_en;
+			func3_reg <= func3;
+		end
+	end
+
 end
 
 endmodule
--- Reg_PC.sv
+++ Reg_PC.sv
@@ -1,12 +1,15 @@
 module Reg_PC (
 input clk,
 input reset,
+input stall,
 input [31:0] next_pc,
 output reg [31:0] current_pc
 );
 always @(negedge clk or negedge reset) begin
     if(!reset)
         current_pc <= 32'd0;
+    else if (stall)
+        current_pc <= current_pc;
     else
         current_pc <= next_pc;
 end
